5524a8d5fd8955221ff43ea558a3feb4c408f51c
Sahil Madaan <madaan.sahil27@gmail.com>
2025-05-29 02:35:07 +0530

    rat in a maze

---
+++ 7. Recursion/3. Trying out all combos Hard/4. Rat in a maze/main.cpp
@@ -0,0 +1,108 @@
+// https://takeuforward.org/data-structure/rat-in-a-maze/
+
+// https://takeuforward.org/plus/dsa/problems/rat-in-a-maze
+
+/*
+
+Given a grid of dimensions n x n. A rat is placed at coordinates (0, 0) and wants to reach at coordinates (n-1, n-1).
+
+
+
+Find all possible paths that rat can take to travel from (0, 0) to (n-1, n-1). The directions in which rat can move are 'U' (up) , 'D' (down) , 'L' (left) , 'R' (right).
+
+
+
+The value 0 in grid denotes that the cell is blocked and rat cannot use that cell for travelling, whereas value 1 represents that rat can travel through the cell. If the cell (0, 0) has 0 value, then mouse cannot move to any other cell.
+
+
+Examples:
+Input : n = 4 , grid = [ [1, 0, 0, 0] , [1, 1, 0, 1], [1, 1, 0, 0], [0, 1, 1, 1] ]
+
+Output : [ "DDRDRR" , "DRDDRR" ]
+
+Explanation : The rat has two different path to reach (3, 3).
+
+The first path is (0, 0) => (1, 0) => (2, 0) => (2, 1) => (3, 1) => (3, 2) => (3, 3).
+
+The second path is (0,0) => (1,0) => (1,1) => (2,1) => (3,1) => (3,2) => (3,3).
+
+Input : n = 2 , grid = [ [1, 0] , [1, 0] ]
+
+Output : -1
+
+Explanation : There is no path that rat can choose to travel from (0,0) to (1,1).
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+  void findPathHelper(int i, int j, vector < vector < int >> & a, int n, vector < string > & ans, string move,
+    vector < vector < int >> & vis) {
+    if (i == n - 1 && j == n - 1) {
+      ans.push_back(move);
+      return;
+    }
+
+    // downward
+    if (i + 1 < n && !vis[i + 1][j] && a[i + 1][j] == 1) {
+      vis[i][j] = 1;
+      findPathHelper(i + 1, j, a, n, ans, move + 'D', vis);
+      vis[i][j] = 0;
+    }
+
+    // left
+    if (j - 1 >= 0 && !vis[i][j - 1] && a[i][j - 1] == 1) {
+      vis[i][j] = 1;
+      findPathHelper(i, j - 1, a, n, ans, move + 'L', vis);
+      vis[i][j] = 0;
+    }
+
+    // right 
+    if (j + 1 < n && !vis[i][j + 1] && a[i][j + 1] == 1) {
+      vis[i][j] = 1;
+      findPathHelper(i, j + 1, a, n, ans, move + 'R', vis);
+      vis[i][j] = 0;
+    }
+
+    // upward
+    if (i - 1 >= 0 && !vis[i - 1][j] && a[i - 1][j] == 1) {
+      vis[i][j] = 1;
+      findPathHelper(i - 1, j, a, n, ans, move + 'U', vis);
+      vis[i][j] = 0;
+    }
+
+  }
+  public:
+    vector < string > findPath(vector < vector < int >> & m, int n) {
+      vector < string > ans;
+      vector < vector < int >> vis(n, vector < int > (n, 0));
+
+      if (m[0][0] == 1) findPathHelper(0, 0, m, n, ans, "", vis);
+      return ans;
+    }
+};
+
+int main() {
+  int n = 4;
+
+   vector < vector < int >> m = {{1,0,0,0},{1,1,0,1},{1,1,0,0},{0,1,1,1}};
+
+  Solution obj;
+  vector < string > result = obj.findPath(m, n);
+  if (result.size() == 0)
+    cout << -1;
+  else
+    for (int i = 0; i < result.size(); i++) cout << result[i] << " ";
+  cout << endl;
+
+  return 0;
+}
+Output:
+
+
+// Time Complexity: O(4^(m*n)), because on every cell we need to try 4 different directions.
+
+// Space Complexity:  O(m*n) ,Maximum Depth of the recursion tree(auxiliary space).

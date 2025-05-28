aef011c76a87ab43aee050d76b64d498e150c7ae
Sahil Madaan <madaan.sahil27@gmail.com>
2025-05-29 01:30:03 +0530

    combination sum 3 leetcode 216

--- 7. Recursion/2. Subsequence pattern/11. Combination sum 3/main.cpp
+++ 7. Recursion/2. Subsequence pattern/11. Combination sum 3/main.cpp
@@ -2,3 +2,29 @@
 
 // https://takeuforward.org/plus/dsa/problems/combination-sum-iii?tab=editorial
 
+class Solution {
+    void generate(vector<vector<int>>& ans, vector<int>& curr, int start, int k, int n, int currsum) {
+        if (curr.size() == k) {
+            if (currsum == n) {
+                ans.push_back(curr);
+            }
+            return;
+        }
+
+        for (int i = start; i <= 9; i++) {
+            if (currsum + i > n) break;
+
+            curr.push_back(i);
+            generate(ans, curr, i + 1, k, n, currsum + i);
+            curr.pop_back();
+        }
+    }
+
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        generate(ans, curr, 1, k, n, 0);
+        return ans;
+    }
+};

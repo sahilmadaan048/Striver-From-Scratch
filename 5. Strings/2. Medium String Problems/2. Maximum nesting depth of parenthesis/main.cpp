2a37e9c6a5f1ba09824b1d11856ed8afd851896b
Sahil Madaan <madaan.sahil27@gmail.com>
2025-05-29 11:50:39 +0530

    maximum nesting depth of the parenthesis leetcode 161

---
+++ 5. Strings/2. Medium String Problems/2. Maximum nesting depth of parenthesis/main.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/description/
+
+class Solution {
+public:
+    int maxDepth(string s) {
+        int n = s.size();
+        int maxi = 0;
+        int depth = 0;
+        for(int i=0; i<n ; i++) {
+            if(s[i] == '(') {
+                depth++;
+                maxi = max(maxi, depth);
+            } else if(s[i] == ')') {
+                depth--;
+                maxi = max(maxi, depth);
+            }
+        }
+        return maxi;
+    }
+};

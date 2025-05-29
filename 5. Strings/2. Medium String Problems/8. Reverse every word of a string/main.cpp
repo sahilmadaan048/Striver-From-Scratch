40eb82924a9722044e1a061c857191bb9c93a60c
Sahil Madaan <madaan.sahil27@gmail.com>
2025-05-29 14:43:33 +0530

    reverse words in a string leetcode 151

---
+++ 5. Strings/2. Medium String Problems/8. Reverse every word of a string/main.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/reverse-words-in-a-string/description/
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string temp;
+        vector<string> res;
+
+        while (ss >> temp) {
+            res.push_back(temp);
+        }
+
+        string ans = "";
+        for (int i = res.size() - 1; i >= 0; i--) {
+            ans += res[i];
+            if (i != 0) ans += ' ';
+        }
+
+        return ans;
+    }
+};

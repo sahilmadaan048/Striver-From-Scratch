b4db807041f26c54c189db71b7c9c8b4a774dec5
Sahil Madaan <madaan.sahil27@gmail.com>
2025-06-01 23:49:39 +0530

    Moore Counting algo leetcode 169

---
+++ 3. Solve problem on Arrays/2. medium/3. majority element/main2.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/majority-element/description/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int candidate = 0;
+        
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        
+        return candidate;
+    }
+};

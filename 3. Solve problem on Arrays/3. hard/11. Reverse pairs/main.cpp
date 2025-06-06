b56374fa0e29803cc915ef930e5493d2d48e4c25
Sahil Madaan <madaan.sahil27@gmail.com>
2025-06-06 09:03:12 +0530

    reverse pairs leetcode hahrd 493

---
+++ 3. Solve problem on Arrays/3. hard/11. Reverse pairs/main.cpp
@@ -0,0 +1,41 @@
+// https://takeuforward.org/data-structure/count-reverse-pairs/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int countPairs(vector<int>&a, int n) {
+
+    // Count the number of pairs:
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] > 2 * a[j]) cnt++;
+        }
+    }
+    return cnt;
+}
+
+int team(vector <int> & skill, int n) {
+    return countPairs(skill, n);
+}
+
+int main()
+{
+    vector<int> a = {4, 1, 2, 3, 1};
+    int n = 5;
+    int cnt = team(a, n);
+    cout << "The number of reverse pair is: "
+         << cnt << endl;
+    return 0;
+}
+
+
+/*
+
+Time Complexity: O(N2), where N = size of the given array.
+Reason: We are using nested loops here and those two loops roughly run for N times.
+
+Space Complexity: O(1) as we are not using any extra space to solve this problem.
+
+*/

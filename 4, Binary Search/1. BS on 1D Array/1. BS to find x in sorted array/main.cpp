1e45a75d34c3a53d1bf3f33d7fca1dfcedefa5a9
Sahil Madaan <madaan.sahil27@gmail.com>
2025-06-07 12:12:45 +0530

    Binary search explained

---
+++ 4, Binary Search/1. BS on 1D Array/1. BS to find x in sorted array/main.cpp
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/binary-search/description/
+
+// https://takeuforward.org/data-structure/binary-search-explained/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(vector<int>& nums, int target) {
+    int n = nums.size(); //size of the array
+    int low = 0, high = n - 1;
+
+    // Perform the steps:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) return mid;
+        else if (target > nums[mid]) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> a = {3, 4, 6, 7, 9, 12, 16, 17};
+    int target = 6;
+    int ind = binarySearch(a, target);
+    if (ind == -1) cout << "The target is not present." << endl;
+    else cout << "The target is at index: "
+                  << ind << endl;
+    return 0;
+}
+
+
+/*
+
+In the algorithm, in every step, we are basically dividing the search space into 2 equal halves. This is actually equivalent to dividing the size of the array by 2, every time. After a certain number of divisions, the size will reduce to such an extent that we will not be able to divide that anymore and the process will stop. The number of total divisions will be equal to the time complexity.
+
+Let’s derive the number of divisions mathematically,
+
+If a number n can be divided by 2 for x times:
+	2^x = n
+Therefore, x = logn (base is 2)
+So the overall time complexity is O(logN), where N = size of the given array.
+
+*/
---
+++ 4, Binary Search/1. BS on 1D Array/1. BS to find x in sorted array/main2.cpp
@@ -0,0 +1,43 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(vector<int>& nums, int low, int high, int target) {
+
+    if (low > high) return -1; //Base case.
+
+    // Perform the steps:
+    int mid = (low + high) / 2;
+    if (nums[mid] == target) return mid;
+    else if (target > nums[mid])
+        return binarySearch(nums, mid + 1, high, target);
+    return binarySearch(nums, low, mid - 1, target);
+}
+
+int search(vector<int>& nums, int target) {
+    return binarySearch(nums, 0, nums.size() - 1, target);
+}
+
+int main()
+{
+    vector<int> a = {3, 4, 6, 7, 9, 12, 16, 17};
+    int target = 6;
+    int ind = search(a, target);
+    if (ind == -1) cout << "The target is not present." << endl;
+    else cout << "The target is at index: "
+                  << ind << endl;
+    return 0;
+}
+
+/*
+
+In the algorithm, in every step, we are basically dividing the search space into 2 equal halves. This is actually equivalent to dividing the size of the array by 2, every time. After a certain number of divisions, the size will reduce to such an extent that we will not be able to divide that anymore and the process will stop. The number of total divisions will be equal to the time complexity.
+
+Let’s derive the number of divisions mathematically,
+
+If a number n can be divided by 2 for x times:
+	2^x = n
+Therefore, x = logn (base is 2)
+So the overall time complexity is O(logN), where N = size of the given array.
+
+*/

1714f2087d30b18276b2c58dafb04e0dc6baee9f
Sahil Madaan <madaan.sahil27@gmail.com>
2025-06-07 15:00:43 +0530

    Find minimum in rotated sorted array leetcode med 153

---
+++ 4, Binary Search/1. BS on 1D Array/10. Find minimum in rotated sorted array/main.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+
+// https://takeuforward.org/data-structure/minimum-in-rotated-sorted-array/
+
+class Solution {
+public:
+    int findMin(vector<int>& arr){
+        int low = 0, high = arr.size() - 1;
+        int ans = INT_MAX;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            //if left part is 
+            sorted:
+            if (arr[low] <= arr[mid]) {
+                // keep the minimum:
+                ans = min(ans, arr[low]);
+
+                // Eliminate left half:
+                low = mid + 1;
+            }
+            else { //if right part is sorted:
+
+                // keep the minimum:
+                ans = min(ans, arr[mid]);
+
+                // Eliminate right half:
+                high = mid - 1;
+            }
+        }
+        return ans;
+        }
+};
+
+/*
+
+Time Complexity: O(logN), N = size of the given array.
+Reason: We are basically using binary search to find the minimum.
+
+Space Complexity: O(1)
+Reason: We have not used any extra data structures, this makes space complexity, even in the worst case as O(1).
+
+*/
---
+++ 4, Binary Search/1. BS on 1D Array/10. Find minimum in rotated sorted array/main3.cpp
@@ -0,0 +1,32 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMin(vector<int>& arr) {
+    int n = arr.size(); // size of the array.
+    int mini = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        // Always keep the minimum.
+        mini = min(mini, arr[i]);
+    }
+    return mini;
+}
+
+int main()
+{
+    vector<int> arr = {4, 5, 6, 7, 0, 1, 2, 3};
+    int ans = findMin(arr);
+    cout << "The minimum element is: " << ans << "\n";
+    return 0;
+}
+
+
+/*
+
+Time Complexity: O(N), N = size of the given array.
+Reason: We have to iterate through the entire array to check if the target is present in the array.
+
+Space Complexity: O(1)
+Reason: We have not used any extra data structures, this makes space complexity, even in the worst case as O(1).
+
+*/

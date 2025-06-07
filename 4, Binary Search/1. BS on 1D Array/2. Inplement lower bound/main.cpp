5d2f4b28715127b89802a684e28daae5a6f873a5
Sahil Madaan <madaan.sahil27@gmail.com>
2025-06-07 12:19:06 +0530

    Lower bound in a sotrted array using ilinear and BS

---
+++ 4, Binary Search/1. BS on 1D Array/2. Inplement lower bound/main.cpp
@@ -0,0 +1,34 @@
+// https://takeuforward.org/arrays/implement-lower-bound-bs-2/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int> arr, int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] >= x) {
+            // lower bound found:
+            return i;
+        }
+    }
+    return n;
+}
+
+int main()
+{
+    vector<int> arr = {3, 5, 8, 15, 19};
+    int n = 5, x = 9;
+    int ind = lowerBound(arr, n, x);
+    cout << "The lower bound is the index: " << ind << "\n";
+    return 0;
+}
+
+
+/*
+
+Time Complexity: O(N), where N = size of the given array.
+Reason: In the worst case, we have to travel the whole array. This is basically the time complexity of the linear search algorithm.
+
+Space Complexity: O(1) as we are using no extra space.
+
+*/

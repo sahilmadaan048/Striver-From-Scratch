390761721c84f3857642a956caeb31495dfc28dd
Sahil Madaan <madaan.sahil27@gmail.com>
2025-06-07 13:41:39 +0530

    Search in rotated sorted array 2 leetcode med 81

---
+++ 4, Binary Search/1. BS on 1D Array/9. Search in roteted array 2/main.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
+
+// https://takeuforward.org/arrays/search-element-in-rotated-sorted-array-ii/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool searchInARotatedSortedArrayII(vector<int>&arr, int k) {
+    int n = arr.size(); // size of the array.
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == k) return true;
+    }
+    return false;
+}
+
+int main()
+{
+    vector<int> arr = {7, 8, 1, 2, 3, 3, 3, 4, 5, 6};
+    int k = 3;
+    bool ans = searchInARotatedSortedArrayII(arr, k);
+    if (!ans)
+        cout << "Target is not present.\n";
+    else
+        cout << "Target is present in the array.\n";
+    return 0;
+}
+
+/*
+
+Time Complexity: O(N), N = size of the given array.
+Reason: We have to iterate through the entire array to check if the target is present in the array.
+
+Space Complexity: O(1)
+Reason: We have not used any extra data structures, this makes space complexity, even in the worst case as O(1).
+
+*/

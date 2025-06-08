9401f4be9748e68350127e73aa2b78f9fb708ffd
Sahil Madaan <madaan.sahil27@gmail.com>
2025-06-08 19:41:39 +0530

    Single element in a asorted array leetcode med 540

---
+++ 4, Binary Search/1. BS on 1D Array/12. Single element in a sorted array/main2.cpp
@@ -0,0 +1,31 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int singleNonDuplicate(vector<int>& arr) {
+    int n = arr.size(); //size of the array.
+    int ans = 0;
+    // XOR all the elements:
+    for (int i = 0; i < n; i++) {
+        ans = ans ^ arr[i];
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6};
+    int ans = singleNonDuplicate(arr);
+    cout << "The single element is: " << ans << "\n";
+    return 0;
+}
+
+/*
+Time Complexity: O(N), N = size of the given array.
+Reason: We are traversing the entire array.
+
+Space Complexity: O(1) as we are not using any extra space.
+
+*/

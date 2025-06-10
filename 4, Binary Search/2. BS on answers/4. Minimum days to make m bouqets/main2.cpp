e977235a284857c1e49a8cd8bbfced27c7754af6
Sahil Madaan <madaan.sahil27@gmail.com>
2025-06-10 07:46:04 +0530

    Minimum days to make a bouquet

---
+++ 4, Binary Search/2. BS on answers/4. Minimum days to make m bouqets/main2.cpp
@@ -0,0 +1,68 @@
+// https://takeuforward.org/arrays/minimum-days-to-make-m-bouquets/
+
+// https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/description/
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool possible(vector<int> &arr, int day, int m, int k) {
+    int n = arr.size(); //size of the array
+    int cnt = 0;
+    int noOfB = 0;
+    // count the number of bouquets:
+    for (int i = 0; i < n; i++) {
+        if (arr[i] <= day) {
+            cnt++;
+        }
+        else {
+            noOfB += (cnt / k);
+            cnt = 0;
+        }
+    }
+    noOfB += (cnt / k);
+    return noOfB >= m;
+}
+int roseGarden(vector<int> arr, int k, int m) {
+    long long val = m * 1ll * k * 1ll;
+    int n = arr.size(); //size of the array
+    if (val > n) return -1; //impossible case.
+    //find maximum and minimum:
+    int mini = INT_MAX, maxi = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        mini = min(mini, arr[i]);
+        maxi = max(maxi, arr[i]);
+    }
+
+    for (int i = mini; i <= maxi; i++) {
+        if (possible(arr, i, m, k))
+            return i;
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {7, 7, 7, 7, 13, 11, 12, 7};
+    int k = 3;
+    int m = 2;
+    int ans = roseGarden(arr, k, m);
+    if (ans == -1)
+        cout << "We cannot make m bouquets.\n";
+    else
+        cout << "We can make bouquets on day " << ans << "\n";
+    return 0;
+}
+
+/*
+Time Complexity: O((max(arr[])-min(arr[])+1) * N), where {max(arr[]) -> maximum element of the array, min(arr[]) -> minimum element of the array, N = size of the array}.
+Reason: We are running a loop to check our answers that are in the range of [min(arr[]), max(arr[])]. For every possible answer, we will call the possible() function. Inside the possible() function, we are traversing the entire array, which results in O(N).
+
+Space Complexity: O(1) as we are not using any extra space to solve this problem.
+
+
+
+
+*/

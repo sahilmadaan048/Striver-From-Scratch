43ca216d93a516ecaac7717310194fb78114328e
Sahil Madaan <madaan.sahil27@gmail.com>
2025-06-11 21:43:23 +0530

    Split array largest sum leetcode hard 410

---
+++ 4, Binary Search/2. BS on answers/10. Split arrayLargest sum/main2.cpp
@@ -0,0 +1,54 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int countPartitions(vector<int> &a, int maxSum) {
+    int n = a.size(); //size of array.
+    int partitions = 1;
+    long long subarraySum = 0;
+    for (int i = 0; i < n; i++) {
+        if (subarraySum + a[i] <= maxSum) {
+            //insert element to current subarray
+            subarraySum += a[i];
+        }
+        else {
+            //insert element to next subarray
+            partitions++;
+            subarraySum = a[i];
+        }
+    }
+    return partitions;
+}
+
+int largestSubarraySumMinimized(vector<int> &a, int k) {
+    int low = *max_element(a.begin(), a.end());
+    int high = accumulate(a.begin(), a.end(), 0);
+
+    for (int maxSum = low; maxSum <= high; maxSum++) {
+        if (countPartitions(a, maxSum) == k)
+            return maxSum;
+    }
+    return low;
+}
+
+int main()
+{
+    vector<int> a = {10, 20, 30, 40};
+    int k = 2;
+    int ans = largestSubarraySumMinimized(a, k);
+    cout << "The answer is: " << ans << "\n";
+    return 0;
+}
+
+
+/*
+Time Complexity: O(N * (sum(arr[])-max(arr[])+1)), where N = size of the array, sum(arr[]) = sum of all array elements, max(arr[]) = maximum of all array elements.
+Reason: We are using a loop from max(arr[]) to sum(arr[]) to check all possible values of time. Inside the loop, we are calling the countPartitions() function for each number. Now, inside the countPartitions() function, we are using a loop that runs for N times.
+
+Space Complexity:  O(1) as we are not using any extra space to solve this problem.
+
+
+
+*/

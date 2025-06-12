f0f0852326de1ba64d76cd219e6e061484c59e14
Sahil Madaan <madaan.sahil27@gmail.com>
2025-06-12 22:30:58 +0530

    pow x n leetcode med 50

---
+++ 8. Bit Manipulation/lec3. Advanced Maths/5. Power x n/main.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/powx-n/
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double result = 1;
+        long long abs_n = n; 
+        if (n < 0) {
+            abs_n = -abs_n;
+            x = 1 / x;  // If n is negative, invert the base
+        }
+
+        while (abs_n) {
+            if (abs_n % 2 == 1) {  // If n is odd
+                result *= x;
+            }
+            x *= x;  // Square the base
+            abs_n /= 2;  // Halve the exponent
+        }
+        return result;
+    }
+};

df91ffa5aacef47a2f2d0cd300f8180299f7e994
Sahil Madaan <madaan.sahil27@gmail.com>
2025-06-13 20:08:39 +0530

    leetcode hard 4. Median of Two Sorted Arrays

---
+++ 4, Binary Search/2. BS on answers/13. Median of 2 sorted arrays/main2.cpp
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+
+// https://takeuforward.org/data-structure/median-of-two-sorted-arrays-of-different-sizes/
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+double median(vector<int>& a, vector<int>& b) {
+    //size of two given arrays:
+    int n1 = a.size(), n2 = b.size();
+
+    vector<int> arr3;
+    //apply the merge step:
+    int i = 0, j = 0;
+    while (i < n1 && j < n2) {
+        if (a[i] < b[j]) arr3.push_back(a[i++]);
+        else arr3.push_back(b[j++]);
+    }
+
+    //copy the left-out elements:
+    while (i < n1) arr3.push_back(a[i++]);
+    while (j < n2) arr3.push_back(b[j++]);
+
+    //Find the median:
+    int n = n1 + n2;
+    if (n % 2 == 1) {
+        return (double)arr3[n / 2];
+    }
+
+    double median = ((double)arr3[n / 2] + (double)arr3[(n / 2) - 1]) / 2.0;
+    return median;
+}
+
+int main()
+{
+    vector<int> a = {1, 4, 7, 10, 12};
+    vector<int> b = {2, 3, 6, 15};
+    cout << "The median of two sorted array is " << fixed << setprecision(1)
+         << median(a, b) << '\n';
+}
+
+/*
+
+Time Complexity: O(n1+n2), where  n1 and n2 are the sizes of the given arrays.
+Reason: We traverse through both arrays linearly.
+
+Space Complexity: O(n1+n2), where  n1 and n2 are the sizes of the given arrays.
+Reason: We are using an extra array of size (n1+n2) to solve this problem.
+
+*/
---
+++ 4, Binary Search/2. BS on answers/13. Median of 2 sorted arrays/main4.cpp
@@ -0,0 +1,67 @@
+class Solution {
+    void merge(int n1, int n2, vector<int>& nums1, vector<int>& nums2, vector<int>& merged) {
+        int i = 0, j = 0;
+        while (i < n1 && j < n2) {
+            if (nums1[i] < nums2[j]) {
+                merged.push_back(nums1[i++]);
+            } else {
+                merged.push_back(nums2[j++]);
+            }
+        }
+        while (i < n1) {
+            merged.push_back(nums1[i++]);
+        }
+        while (j < n2) {
+            merged.push_back(nums2[j++]);
+        }
+    }
+
+
+double median(vector<int>& a, vector<int>& b) {
+    int n1 = a.size(), n2 = b.size();
+    //if n1 is bigger swap the arrays:
+    if (n1 > n2) return median(b, a);
+
+    int n = n1 + n2; //total length
+    int left = (n1 + n2 + 1) / 2; //length of left half
+    //apply binary search:
+    int low = 0, high = n1;
+    while (low <= high) {
+        int mid1 = (low + high) >> 1;
+        int mid2 = left - mid1;
+        //calculate l1, l2, r1 and r2;
+        int l1 = INT_MIN, l2 = INT_MIN;
+        int r1 = INT_MAX, r2 = INT_MAX;
+        if (mid1 < n1) r1 = a[mid1];
+        if (mid2 < n2) r2 = b[mid2];
+        if (mid1 - 1 >= 0) l1 = a[mid1 - 1];
+        if (mid2 - 1 >= 0) l2 = b[mid2 - 1];
+
+        if (l1 <= r2 && l2 <= r1) {
+            if (n % 2 == 1) return max(l1, l2);
+            else return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+        }
+
+        //eliminate the halves:
+        else if (l1 > r2) high = mid1 - 1;
+        else low = mid1 + 1;
+    }
+    return 0; //dummy statement
+}
+
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        // int n1 = nums1.size();
+        // int n2 = nums2.size();
+
+        // vector<int> merged;
+        // merge(n1, n2, nums1, nums2, merged);
+
+        // int n = n1 + n2;
+        // if (n % 2 == 1) {
+        //     return merged[n / 2];
+        // } 
+        // return (merged[n / 2 - 1] + merged[n / 2]) / 2.0;
+        return median(nums1, nums2);
+    }
+};

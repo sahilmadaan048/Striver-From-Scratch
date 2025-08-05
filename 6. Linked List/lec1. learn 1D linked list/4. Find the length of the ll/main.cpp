a25ef5f77ede2d0284ebbd3d873067dbf1b01796
Sahil Madaan <madaan.sahil27@gmail.com>
2025-08-05 12:37:41 +0530

    Length of Linked List done

---
+++ 6. Linked List/lec1. learn 1D linked list/4. Find the length of the ll/main.cpp
@@ -0,0 +1,44 @@
+// https://takeuforward.org/linked-list/find-the-length-of-a-linked-list
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    public:
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
+    }
+    public:
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+int lengthOfLinkedList(Node* head) {
+    Node* temp = head;
+    int cnt = 0;
+
+    while (temp != NULL) {
+        temp = temp->next;
+        cnt++; // increment cnt for every node traversed
+    }
+    return cnt;
+}
+
+int main() {
+    vector<int> arr = {2, 5, 8, 7};
+    
+    Node* head = new Node(arr[0]);
+    
+    head->next = new Node(arr[1]);
+    head->next->next = new Node(arr[2]);
+    head->next->next->next = new Node(arr[3]);
+    
+    cout << lengthOfLinkedList(head) << '\n';
+}

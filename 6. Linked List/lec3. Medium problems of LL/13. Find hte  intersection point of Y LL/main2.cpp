f3cd51fba708dff831e5c728276926cd001f71f6
Sahil Madaan <madaan.sahil27@gmail.com>
2025-08-06 13:17:48 +0530

    160. Intersection of Two Linked Lists

---
+++ 6. Linked List/lec3. Medium problems of LL/13. Find hte  intersection point of Y LL/main2.cpp
@@ -0,0 +1,98 @@
+// https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+
+// https://takeuforward.org/data-structure/find-intersection-of-two-linked-lists/
+
+// https://www.youtube.com/watch?v=u4FWXfgS8jw
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class node {
+    public:
+        int num;
+        node* next;
+        node(int val) {
+            num = val;
+            next = NULL;
+        }
+};
+//utility function to insert node at the end of the linked list
+void insertNode(node* &head,int val) {
+    node* newNode = new node(val);
+    
+    if(head == NULL) {
+        head = newNode;
+        return;
+    }
+    
+    node* temp = head;
+    while(temp->next != NULL) temp = temp->next;
+    
+    temp->next = newNode;
+    return;
+}
+
+//utility function to check presence of intersection
+node* intersectionPresent(node* head1,node* head2) {
+     unordered_set<node*> st;
+    while(head1 != NULL) {
+       st.insert(head1);
+       head1 = head1->next;
+    }
+    while(head2 != NULL) {
+        if(st.find(head2) != st.end()) return head2;
+        head2 = head2->next;
+    }
+    return NULL;
+
+}
+
+//utility function to print linked list created
+void printList(node* head) {
+    while(head->next != NULL) {
+        cout<<head->num<<"->";
+        head = head->next;
+    }
+    cout<<head->num<<endl;
+}
+
+int main() {
+    // creation of both lists 
+    node* head = NULL;
+    insertNode(head,1);
+    insertNode(head,3);
+    insertNode(head,1);
+    insertNode(head,2);
+    insertNode(head,4);
+    node* head1 = head;
+    head = head->next->next->next;
+    node* headSec = NULL;
+    insertNode(headSec,3);
+    node* head2 = headSec;
+    headSec->next = head;
+    //printing of the lists
+    cout<<"List1: "; printList(head1);
+    cout<<"List2: "; printList(head2);
+    //checking if intersection is present
+    node* answerNode = intersectionPresent(head1,head2);
+    if(answerNode == NULL )
+    cout<<"No intersection\n";
+    else
+    cout<<"The intersection point is "<<answerNode->num<<endl;
+    return 0;
+}
+
+
+/*
+
+Time Complexity: O(n+m)
+
+Reason: Iterating through list 1 first takes O(n), then iterating through list 2 takes O(m). 
+
+Space Complexity: O(n)
+
+Reason: Storing list 1 node addresses in unordered_set.
+
+
+*/

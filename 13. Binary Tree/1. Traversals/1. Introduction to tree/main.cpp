ee416df3506e078066c11bc96104325dedb50ca3
Sahil Madaan <madaan.sahil27@gmail.com>
2025-08-07 16:23:45 +0530

    Introduction to Trees

---
+++ 13. Binary Tree/1. Traversals/1. Introduction to tree/main.cpp
@@ -0,0 +1,12 @@
+// https://takeuforward.org/binary-tree/introduction-to-trees/
+
+Binary Tree 
+Nodes 
+Root Node 
+Children nodes
+Leaf Nodes 
+Full Binary Tree - each node has either 0 or 2 nodes 
+Complete Binary Tree - every level is completely filled except possibly the last level 
+Perfect Binary Tree - all the leaf nodes are at the same level and the number od leaf nodes is maximised on that level 
+Balanced Binary Tree - where the heighjts of the 2 subtrees of any node differ by at most one 
+Degenate Tree - where the nodes are arranges in sa single path leaninf to the right or left

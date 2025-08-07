facb6569c972fa9ecb9b08bc7ac71ba1d60d0339
Sahil Madaan <madaan.sahil27@gmail.com>
2025-08-07 18:08:28 +0530

    145. Binary Tree Postorder Traversal

---
+++ 13. Binary Tree/1. Traversals/11. Postorder traversal of BT using 2 stacks/main.cpp
@@ -0,0 +1,42 @@
+// https://takeuforward.org/data-structure/iterative-postorder-traversal-of-binary-tree-using-2-stack
+
+// https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if (!root) return ans;
+
+        stack<TreeNode*> st1, st2;
+        st1.push(root);
+
+        while (!st1.empty()) {
+            TreeNode* node = st1.top();
+            st1.pop();
+            st2.push(node);
+
+            if (node->left) st1.push(node->left);
+            if (node->right) st1.push(node->right);
+        }
+
+        while (!st2.empty()) {
+            TreeNode* node = st2.top();
+            st2.pop();
+            ans.push_back(node->val);
+        }
+
+        return ans;
+    }
+};

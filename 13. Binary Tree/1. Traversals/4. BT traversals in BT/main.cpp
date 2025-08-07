f7891daec36b0f5f0a3a180064e18bd16ed6fa23
Sahil Madaan <madaan.sahil27@gmail.com>
2025-08-07 16:34:57 +0530

    Binary Tree Traversal : Inorder Preorder Postorder

---
+++ 13. Binary Tree/1. Traversals/4. BT traversals in BT/main.cpp
@@ -0,0 +1,17 @@
+// https://takeuforward.org/binary-tree/binary-tree-traversal-inorder-preorder-postorder/
+
+We learnt about DFS 
+We learnt about BFS 
+    
+Inorder Traversal
+https://takeuforward.org/data-structure/inorder-traversal-of-binary-tree
+
+Preorder Traversal
+https://takeuforward.org/data-structure/preorder-traversal-of-binary-tree
+
+Postorder Traversal
+https://takeuforward.org/data-structure/post-order-traversal-of-binary-tree
+
+Level Order Traversal
+https://takeuforward.org/data-structure/level-order-traversal-of-a-binary-tree
+

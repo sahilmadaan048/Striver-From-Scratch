1c6811381594c706803e174fcb0325292c8ef953
Sahil Madaan <madaan.sahil27@gmail.com>
2025-08-08 15:22:35 +0530

    701. Insert into a Binary Search Tree

---
+++ 14. Binary Search Trees/2. Practive Problems/3. Insert a given Node in BST/main.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/insert-into-a-binary-search-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root == nullptr) return new TreeNode(val);
+
+        TreeNode* dummy = root;
+
+        while(true) {
+            if(val < (root->val)) {
+                if(root->left == NULL) {
+                    root->left = new TreeNode(val); 
+                    break;
+                } else {
+                    root = root->left;
+                }
+            }
+            else {
+                if(root->right == NULL) {
+                    root->right = new TreeNode(val);
+                    break;
+                } else {
+                    root = root->right;
+                }
+            }
+        }
+
+        return dummy;
+    }
+};
+

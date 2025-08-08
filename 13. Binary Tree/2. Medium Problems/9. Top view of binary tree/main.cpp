c3d4bf705308e37c50acc6e3331fd5a8c3ef7057
Sahil Madaan <madaan.sahil27@gmail.com>
2025-08-09 00:52:06 +0530

    Top View of Binary Tree

---
+++ 13. Binary Tree/2. Medium Problems/9. Top view of binary tree/main.cpp
@@ -0,0 +1,67 @@
+// https://takeuforward.org/data-structure/top-view-of-a-binary-tree/
+
+// https://www.geeksforgeeks.org/problems/top-view-of-binary-tree/1
+
+
+/*
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+*/
+class Solution {
+  public:
+    // Function to return a list of nodes visible from the top view
+    // from left to right in Binary Tree.
+    vector<int> topView(Node *root) {
+        vector<int> ans;
+        
+        if(root == nullptr) {
+            return ans;
+        }
+        
+        map<int, int> mpp; 
+        queue<pair<Node*, int>> q; 
+        
+        q.push({root, 0});
+        
+        while(!q.empty()) {
+            auto it = q.front();
+            q.pop();
+            
+            Node* node = it.first;
+            int line = it.second;
+            
+            if(mpp.find(line) == mpp.end()) {
+                mpp[line] = node->data;
+            }
+            
+            if(node->left != nullptr) {
+                q.push({node->left, line - 1});
+            }
+            if(node->right != nullptr) {
+                q.push({node->right, line + 1});
+            }
+        }
+        
+        for(auto& it : mpp) {
+            ans.push_back(it.second);
+        }
+            
+        return ans;
+    }
+};
+
+
+/*
+
+Time Complexity: O(N) where N is the number of nodes in the Binary Tree. This complexity arises from visiting each node exactly once during the BFS traversal.
+
+Space Complexity: O(N/2 + N/2) where N represents the number of nodes in the Binary Tree.
+
+The main space consuming data structure is the queue used for BFS traversal. It acquires space proportional to the number of nodes in the level it is exploring hence in the worst case of a balanced binary tree, the queue will have at most N/2 nodes which is the maximum width.
+Additionally, the map is used to store the top view nodes based on their vertical positions hence its complexity will also be proportional to the greatest width level. In the worst case, it may have N/2 entries as well.
+
+*/

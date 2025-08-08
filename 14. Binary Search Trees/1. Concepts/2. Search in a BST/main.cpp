ae6214db3340f228e735578ae153b64136aa5adf
Sahil Madaan <madaan.sahil27@gmail.com>
2025-08-08 15:07:53 +0530

    700. Search in a Binary Search Treea

---
+++ 14. Binary Search Trees/1. Concepts/2. Search in a BST/main.cpp
@@ -0,0 +1,33 @@
+// https://takeuforward.org/data-structure/search-in-a-binary-search-tree-2/
+
+// https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while(root != NULL && root->val != val){
+            root = val < root->val ? root->left : root->right;
+        }
+        return root;
+    }
+};
+
+
+/*
+
+Time Complexity: O(log2N) where N is the number of nodes in the Binary Search Tree. In the best case scenario, where the tree is balanced, the time complexity is the height of the tree ie. log2N. In the worst-case scenario, where the tree is degenerate (linear), the time complexity becomes O(n), as it would require traversing all nodes along the path from the root to the leaf.
+
+Space Complexity: O(1) since the algorithm does not use any additional space or data structures. The algorithm does use auxiliary stack space from recursion. In the average and worst-case scenarios, the space complexity for recursive stack space is O(h), where 'h' represents the height of the tree.
+
+*/

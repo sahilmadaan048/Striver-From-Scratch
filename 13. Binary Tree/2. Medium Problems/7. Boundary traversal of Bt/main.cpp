fae30a767122dd80e0e71a5dd32444411acac492
Sahil Madaan <madaan.sahil27@gmail.com>
2025-08-08 23:52:14 +0530

    Boundary Traversal done

--- 13. Binary Tree/2. Medium Problems/7. Boundary traversal of Bt/main.cpp
+++ 13. Binary Tree/2. Medium Problems/7. Boundary traversal of Bt/main.cpp
@@ -1,13 +1,8 @@
 // https://takeuforward.org/data-structure/boundary-traversal-of-a-binary-tree/
 
-
 // https://takeuforward.org/data-structure/boundary-traversal-of-a-binary-tree/
 
-
 // https://takeuforward.org/plus/dsa/problems/boundary-traversal
-
-
-
 
 #include <iostream>
 #include <vector>
@@ -15,38 +10,47 @@
 using namespace std;
 
 // Node structure for the binary tree
-struct Node {
+struct Node
+{
     int data;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
     // Constructor to initialize
     // the node with a value
     Node(int val) : data(val), left(nullptr), right(nullptr) {}
 };
 
-class Solution {
+class Solution
+{
 public:
     // Function to check
     // if a node is a leaf
-    bool isLeaf(Node* root) {
+    bool isLeaf(Node *root)
+    {
         return !root->left && !root->right;
     }
 
     // Function to add the
     // left boundary of the tree
-    void addLeftBoundary(Node* root, vector<int>& res) {
-        Node* curr = root->left;
-        while (curr) {
+    void addLeftBoundary(Node *root, vector<int> &res)
+    {
+        Node *curr = root->left;
+        while (curr)
+        {
             // If the current node is not a leaf,
             // add its value to the result
-            if (!isLeaf(curr)) {
+            if (!isLeaf(curr))
+            {
                 res.push_back(curr->data);
             }
             // Move to the left child if it exists,
             // otherwise move to the right child
-            if (curr->left) {
+            if (curr->left)
+            {
                 curr = curr->left;
-            } else {
+            }
+            else
+            {
                 curr = curr->right;
             }
         }
@@ -54,59 +58,73 @@ public:
 
     // Function to add the
     // right boundary of the tree
-    void addRightBoundary(Node* root, vector<int>& res) {
-        Node* curr = root->right;
+    void addRightBoundary(Node *root, vector<int> &res)
+    {
+        Node *curr = root->right;
         vector<int> temp;
-        while (curr) {
+        while (curr)
+        {
             // If the current node is not a leaf,
             // add its value to a temporary vector
-            if (!isLeaf(curr)) {
+            if (!isLeaf(curr))
+            {
                 temp.push_back(curr->data);
             }
             // Move to the right child if it exists,
             // otherwise move to the left child
-            if (curr->right) {
+            if (curr->right)
+            {
                 curr = curr->right;
-            } else {
+            }
+            else
+            {
                 curr = curr->left;
             }
         }
         // Reverse and add the values from
         // the temporary vector to the result
-        for (int i = temp.size() - 1; i >= 0; --i) {
+        for (int i = temp.size() - 1; i >= 0; --i)
+        {
             res.push_back(temp[i]);
         }
     }
 
     // Function to add the
     // leaves of the tree
-    void addLeaves(Node* root, vector<int>& res) {
+    void addLeaves(Node *root, vector<int> &res)
+    {
         // If the current node is a
         // leaf, add its value to the result
-        if (isLeaf(root)) {
+        if (isLeaf(root))
+        {
             res.push_back(root->data);
             return;
         }
         // Recursively add leaves of
         // the left and right subtrees
-        if (root->left) {
+        if (root->left)
+        {
             addLeaves(root->left, res);
         }
-        if (root->right) {
+        if (root->right)
+        {
             addLeaves(root->right, res);
         }
     }
 
     // Main function to perform the
     // boundary traversal of the binary tree
-    vector<int> printBoundary(Node* root) {
+    vector<int> printBoundary(Node *root)
+    {
         vector<int> res;
-        if (!root) {
+        if (!root)
+        {
             return res;
         }
         // If the root is not a leaf,
         // add its value to the result
-        if (!isLeaf(root)) {
+        if (!isLeaf(root))
+        {
             res.push_back(root->data);
         }
 
@@ -122,16 +140,19 @@ public:
 
 // Helper function to
 // print the result
-void printResult(const vector<int>& result) {
-    for (int val : result) {
+void printResult(const vector<int> &result)
+{
+    for (int val : result)
+    {
         cout << val << " ";
     }
     cout << endl;
 }
 
-int main() {
+int main()
+{
     // Creating a sample binary tree
-    Node* root = new Node(1);
+    Node *root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
     root->left->left = new Node(4);
@@ -150,8 +171,6 @@ int main() {
 
     return 0;
 }
-                            
-
 
 /*
 

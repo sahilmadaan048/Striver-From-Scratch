04f78b2c801ce6777f8b73ca56b2a8d646ca6247
Sahil Madaan <madaan.sahil27@gmail.com>
2025-08-09 07:53:35 +0530

    Print path from root to a given node in a binary tree

--- 13. Binary Tree/3. Hard Problems/1. Root to Node path in BT/main.cpp
+++ 13. Binary Tree/3. Hard Problems/1. Root to Node path in BT/main.cpp
@@ -96,3 +96,15 @@ int main()
     printPath(root, x);
     return 0;
 }
+
+
+/*
+
+Time Complexity: O(N) where N is the number of nodes in the binary tree as each node of the binary tree is visited exactly once in the inorder traversal.
+
+Space Complexity: O(N) awhere N is the number of nodes in the binary tree. This is because the stack can potentially hold all nodes in the tree when dealing with a skewed tree (all nodes have only one child), consuming space proportional to the number of nodes.
+
+O(H): In the average case or for a balanced tree, the maximum number of nodes that could be in the stack at any given time would be roughly the height of the tree hence O(log2N).
+
+
+*/

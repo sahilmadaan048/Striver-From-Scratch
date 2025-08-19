f64329249b43ced6ba242ca911268a20eab99376
Sahil Madaan <madaan.sahil27@gmail.com>
2025-08-19 08:49:50 +0530

    7. Asteriod collision

---
+++ 9. Stack and Queue/3. Monotonic Stack and queue problems/7. Asteriod collision/main.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/asteroid-collision/description/
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        int n = asteroids.size();
+        stack<int> st;
+
+        for (int i = 0; i < n; i++) {
+            int a = asteroids[i];
+
+            if (a > 0) {
+                st.push(a);
+            } else {
+                // Handle collisions
+                while (!st.empty() && st.top() > 0 && st.top() < -a) {
+                    st.pop();
+                }
+
+                if (st.empty() || st.top() < 0) {
+                    st.push(a);
+                } else if (st.top() == -a) {
+                    st.pop(); // both explode
+                }
+            }
+        }
+
+        // Transfer stack to result vector (in reverse order)
+        vector<int> res(st.size());
+        int i = st.size() - 1;
+        while (!st.empty()) {
+            res[i--] = st.top();
+            st.pop();
+        }
+        return res;
+    }
+};

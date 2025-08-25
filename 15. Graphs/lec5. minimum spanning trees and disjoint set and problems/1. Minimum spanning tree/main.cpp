99d798b2a6ada574f9cc6bfc2baa1d55d033b166
Sahil Madaan <madaan.sahil27@gmail.com>
2025-08-25 12:18:23 +0530

    What is MST

---
+++ 15. Graphs/lec5. minimum spanning trees and disjoint set and problems/1. Minimum spanning tree/main.cpp
@@ -0,0 +1,7 @@
+// https://takeuforward.org/data-structure/minimum-spanning-tree-theory-g-44/
+
+there are a couple of algorithms that help us to find the minimum spanning
+tree of a grapph
+one is prim's algorithm and the other is kruskal's algorithm
+
+we will learn both of them

0e4d90f79ef3d421c5a5e14a3a1e6b9de374afc7
Sahil Madaan <madaan.sahil27@gmail.com>
2025-11-06 00:07:26 +0530

    15. Graphs - shortest path in UG with unit weights

---
+++ 15. Graphs/lec4. shortest path algorithms and problems/1. Shortest path in UG with unit weights/main.cpp
@@ -0,0 +1,83 @@
+// https://takeuforward.org/data-structure/shortest-path-in-undirected-graph-with-unit-distance-g-28/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> shortestPath(vector<vector<int>> &edges, int N, int M, int src)
+    {
+        // Create an adjacency list of size N for storing the undirected graph.
+        vector<int> adj[N];
+        for (auto it : edges)
+        {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+
+        // A dist array of size N initialised with a large number to
+        // indicate that initially all the nodes are untraversed.
+
+        int dist[N];
+        for (int i = 0; i < N; i++)
+            dist[i] = 1e9;
+        // BFS Implementation.
+        dist[src] = 0;
+        queue<int> q;
+        q.push(src);
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            for (auto it : adj[node])
+            {
+                if (dist[node] + 1 < dist[it])
+                {
+                    dist[it] = 1 + dist[node];
+                    q.push(it);
+                }
+            }
+        }
+        // Updated shortest distances are stored in the resultant array ‘ans’.
+        // Unreachable nodes are marked as -1.
+        vector<int> ans(N, -1);
+        for (int i = 0; i < N; i++)
+        {
+            if (dist[i] != 1e9)
+            {
+                ans[i] = dist[i];
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+
+    int N = 9, M = 10;
+    vector<vector<int>> edges = {{0, 1}, {0, 3}, {3, 4}, {4, 5}, {5, 6}, {1, 2}, {2, 6}, {6, 7}, {7, 8}, {6, 8}};
+
+    Solution obj;
+    vector<int> ans = obj.shortestPath(edges, N, M, 0);
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+
+        cout << ans[i] << " ";
+    }
+
+    return 0;
+}
+// Output:
+
+// 0 1 2 1 2 3 3 4 4
+
+// Time Complexity: O(M) { for creating the adjacency list from given list ‘edges’} + O(N + 2M) { for the BFS Algorithm} + O(N) { for adding the final values of the shortest path in the resultant array} ~ O(N+2M).
+
+// Where N= number of vertices and M= number of edges.
+
+// Space Complexity:  O( N) {for the stack storing the BFS} + O(N) {for the resultant array} + O(N) {for the dist array storing updated shortest paths} + O( N+2M) {for the adjacency list} ~ O(N+M) .
+
+// Where N= number of vertices and M= number of edges.

a9a01327efb2f93373c3a9a72b9029d86b85f39e
Sahil Madaan <madaan.sahil27@gmail.com>
2025-11-05 16:50:50 +0530

    15. Graphs - course schedule 2

---
+++ 15. Graphs/lec3 topo sort and problems/5. course schedule 2/main.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/course-schedule-ii/description/
+
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indegree(numCourses, 0);
+        vector<vector<int>> adj(numCourses);
+        for (auto& it : prerequisites) {
+            int course = it[0]; // course to take
+            int pre = it[1];    // prerequisite
+            adj[pre].push_back(course);
+            indegree[course]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+        vector<int> topo;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for (auto it : adj[node]) {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+        if (topo.size() == numCourses)
+            return topo;
+        return {};
+    }
+};

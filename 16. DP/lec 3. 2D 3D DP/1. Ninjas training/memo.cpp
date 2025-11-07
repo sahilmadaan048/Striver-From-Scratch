edb83b10c39564ac5859d752954908726c70c646
Sahil Madaan <madaan.sahil27@gmail.com>
2025-11-07 12:41:36 +0530

    16. DP - ninja training

---
+++ 16. DP/lec 3. 2D 3D DP/1. Ninjas training/memo.cpp
@@ -0,0 +1,52 @@
+// https://takeuforward.org/data-structure/dynamic-programming-ninjas-training-dp-7/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int f(int day, int last, vector<vector<int>> &points, vector<vector<int>> &dp) {
+  if (dp[day][last] != -1) return dp[day][last];
+
+  if (day == 0) {
+    int maxi = 0;   
+    for (int i = 0; i <= 2; i++) {
+      if (i != last)
+        maxi = max(maxi, points[0][i]);
+    }
+    return dp[day][last] = maxi;
+  }
+
+  int maxi = 0;
+  for (int i = 0; i <= 2; i++) {
+    if (i != last) {
+      int activity = points[day][i] + f(day - 1, i, points, dp);
+      maxi = max(maxi, activity);
+    }
+  }
+
+  return dp[day][last] = maxi;
+}
+
+int ninjaTraining(int n, vector<vector<int>> &points) {
+  vector<vector<int>> dp(n, vector<int>(4, -1));
+  return f(n - 1, 3, points, dp);
+}
+
+int main() {
+  vector<vector<int>> points = {{10, 40, 70},
+                                 {20, 50, 80},
+                                 {30, 60, 90}};
+
+  int n = points.size();  
+  cout << ninjaTraining(n, points);
+}
+
+
+/*
+Time Complexity: O(N*4*3)
+
+Reason: There are N*4 states and for every state, we are running a for loop iterating three times.
+
+Space Complexity: O(N) + O(N*4)
+
+Reason: We are using a recursion stack space(O(N)) and a 2D array (again O(N*4)). Therefore total space complexity will be O(N) + O(N) ≈ O(N)
+*/

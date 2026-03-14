6c079db5f169fa20d27153bb0934d850c31cda9b
Sahil Madaan <madaan.sahil27@gmail.com>
2026-03-14 17:37:23 +0530

    feat: Introduction to BST

--- 14. Binary Search Trees/1. Concepts/1. Introduction to BST/main.cpp
+++ 14. Binary Search Trees/1. Concepts/1. Introduction to BST/main.cpp
@@ -3,3 +3,7 @@
 // For every given node in a Binary Search Tree if a left or a right child exists then:
 
 // Left Child < Node < Right Child
+
+// its needed becauise the maxixmum height in almost all cases is kept in order of log N 
+
+// while in case of simple BT it reaches O(n) in case of skew trees
